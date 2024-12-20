1b14084a5f526716177858a36ad4af346520598f
yeo99 <qw6375@gmail.com>
2024-12-20 04:48:58 +0000

    Chore: 환경변수 변경

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_VITE_BACKEND_LOCAL_SERVER_URL,
-    timeout: 5000,
+    baseURL: import.meta.env.VITE_BACKEND_SERVER_URL,
+    timeout: 10000,
     headers: {
         "Content-Type": "application/json",
     }
