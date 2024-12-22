5b8ed54532f9b6d57b824d13ecb3a2f57faebc04
yeo99 <qw6375@gmail.com>
2024-12-22 13:57:00 +0000

    #9 Feat: 로그인 기능 구현중 (authCode fetch 요청 부분 수정 필요)

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,10 +1,57 @@
 import styled from 'styled-components';
 import { FcGoogle } from 'react-icons/fc';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login: React.FC = () => {
+  const navigate = useNavigate(); 
+
   const handleGoogleLogin = () => {
-    console.log("구글 로그인 클릭됨");
+    const url = `https://accounts.google.com/o/oauth2/auth?` +
+      `client_id=${import.meta.env.VITE_GOOGLE_CLIENT_ID}&` +
+      `redirect_uri=${import.meta.env.VITE_REDIRECT_URI}&` +
+      `response_type=code&` +
+      `scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile`;
+    window.location.href = url;
   };
+
+  useEffect(() => {
+    const query = new URLSearchParams(window.location.search);
+    const code = query.get('code');
+    
+    if (code) {
+      const fetchUserData = async (code: string) => {
+        try {
+          const response = await fetch('http://localhost:9999/users/login', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ code }),
+          });
+
+          if (response.ok) {
+            const data = await response.json();
+            if (data.token) {
+              console.log('로그인 성공:', data);
+              localStorage.setItem('token', data.token);
+              navigate('/');
+            } else {
+              console.error('로그인 실패: 응답에 토큰이 없습니다.', data);
+            }
+          } else {
+            console.error('HTTP 에러:', response.status);
+            const errorData = await response.json();
+            console.error('에러 메시지:', errorData.message || '에러 메시지가 없습니다.');
+          }
+        } catch (error) {
+          console.error('네트워크 에러:', error);
+        }
+      };
+
+      fetchUserData(code);
+    }
+  }, [navigate]);
 
   return (
     <LoginWrapper>
@@ -24,9 +71,8 @@ const LoginWrapper = styled.div`
   padding-bottom: 100px;
 `;
 
-const Title = styled.text`
+const Title = styled.p`
   font-size: 2rem;
-  fon-weight: lighter;
   margin-bottom: 2rem;
 `;
 
